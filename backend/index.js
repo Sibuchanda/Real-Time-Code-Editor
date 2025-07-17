b889c247ac62022b8d98649a40ae81c728353cf0
Sibuchanda <sibuchanda457@gmail.com>
2025-07-17 22:38:43 +0530

    Real time code editiing is done

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,83 @@
 import express from 'express'
 import http from 'http'
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 8000;
+
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
-io.on("connection",(socket)=>{
-    console.log(`Client Connected.. ID is ${socket.id}`);
-})
+const rooms = new Map();
+
+io.on("connection", (socket) => {
+
+  let currentRoom = null;
+  let currentUser = null;
+
+  socket.on("join", ({ roomId, userName }) => {
+    if (currentRoom) {
+      socket.leave(currentRoom);
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    }
+
+    currentRoom = roomId;
+    currentUser = userName;
+
+    socket.join(roomId);
+
+    if (!rooms.has(roomId)) {
+      rooms.set(roomId, new Set());
+    }
+
+    rooms.get(roomId).add(userName);
+    io.to(roomId).emit("userJoined", Array.from(rooms.get(currentRoom)));
+  });
 
 
-app.listen(PORT, ()=>{
-    console.log(`Listening to PORT ${PORT}...`);
-})
+  socket.on("codeChange", ({ roomId, code }) => {
+    socket.to(roomId).emit("codeUpdate", code);
+  });
+
+
+  socket.on("leaveRoom", () => {
+    if (currentRoom && currentUser) {
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+
+      socket.leave(currentRoom);
+
+      currentRoom = null;
+      currentUser = null;
+    }
+  });
+
+  socket.on("typing", ({ roomId, userName }) => {
+    socket.to(roomId).emit("userTyping", userName);
+  });
+
+  socket.on("languageChange", ({ roomId, language }) => {
+    io.to(roomId).emit("languageUpdate", language);
+  });
+
+  socket.on("disconnect", () => {
+    if (currentRoom && currentUser) {
+      rooms.get(currentRoom).delete(currentUser);
+      io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)));
+    }
+    console.log("user Disconnected");
+  });
+});
+
+
+
+server.listen(PORT, ()=>{
+    console.log("Listening to PORT `${PORT}`");
+}) 
